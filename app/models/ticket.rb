e68b6aa70f04b6d7e449290fb184409655716aad
Max Keller <max@klauskeller.com>
2020-08-25 15:05:22 +0200

    resolved comments

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,5 @@
 class Ticket < ApplicationRecord
-  has_one_attached :photo
+  has_many_attached :photo
   belongs_to :mom, class_name: "User"
   belongs_to :helper, class_name: "User"
   has_many :ticket_categories
