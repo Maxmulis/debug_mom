5d07dce6316ecdedc2b04bdd99a9f9da256a2141
Kristina Zhizherina <diablito.yey@mail.ru>
2020-08-25 13:01:05 +0200

    test page/buttons/imgs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get '/components_test', to: 'pages#components_test' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
