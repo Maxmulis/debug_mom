e5fc86f8a6be52986551616f94172ad40a710208
Ivaylo D <54253699+ipd39952@users.noreply.github.com>
2020-08-26 11:13:21 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
   root to: 'pages#home'
-  resources :tickets, only: [:index, :new, :create] do
-    resources :users, only: [:index, :new, :create]
-  end
+  resources :tickets, only: [:index, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
