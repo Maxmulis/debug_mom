18fb803f3e3a68b780f5a9fde0d07a3eabe0faeb
Max Keller <max@klauskeller.com>
2020-08-26 08:29:32 +0200

    removed simple_form from users#new view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,12 @@
 class UsersController < ApplicationController
   def new
-    # @ticket = Ticket.find(params["ticket_id"])
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
     @user.mom = true
-
-    if @user.save
-      @ticket = Ticket.find(params["ticket_id"])
-      @ticket.mom = params["ticket_id"]
-      redirect_to root_path
-      binding.pry
-    else
-      render :new
-    end
-
+    # to be done
   end
 
   private
