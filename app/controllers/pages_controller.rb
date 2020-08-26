b7eb0b73fef37ff6a99532eb6725e811e6ddae37
marina toto <marinkato@gmail.com>
2020-08-26 13:04:30 +0200

    added success test page and its button

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,9 @@ class PagesController < ApplicationController
   end
 
   def components_test
-    
   end
-  
+
+  def success_page_test
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  get '/components_test', to: 'pages#components_test' 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/components_test', to: 'pages#components_test'
+  get '/success_page_test', to: 'pages#success_page_test' # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
