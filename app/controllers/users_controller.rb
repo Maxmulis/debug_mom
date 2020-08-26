ce07540314c02b49b926504244192a02fbbabf34
Ivaylo D <54253699+ipd39952@users.noreply.github.com>
2020-08-26 11:22:55 +0200

    Update users_controller.rb
    Trying to avoid the custom new and create methods overriding passwordless default ones

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,2 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    @user.mom = true
-    # to be done
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:username, :email)
-  end
 end
