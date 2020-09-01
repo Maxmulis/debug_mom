e508969904226272bc9a8c24c185c43cde6f6e0f
Ivaylo Dyakov <ipd3995@gmail.com>
2020-09-01 09:38:28 +0200

    initial chat setup

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,10 @@ Rails.application.routes.draw do
   get '/success_page_test', to: 'pages#success_page_test' # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :tickets, only: [:index, :new, :create, :show, :edit, :update] do
     resources :users, only: [:index, :new, :create]
+    resources :conversations, only: :show
   end
+
+  resources :conversations, only: :show
 
   get '/components_test', to: 'pages#components_test'
 
