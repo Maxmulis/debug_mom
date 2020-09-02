f1b9e0fe61a870b2260016da1f31d2a8f41dfc0b
Max Keller <max@klauskeller.com>
2020-09-02 09:00:59 +0200

    fixed audio recording (sry Ivo)

--- app/javascript/components/record_voice.js
+++ app/javascript/components/record_voice.js
@@ -48,7 +48,7 @@ const initRecordVideo = () => {
 
   const uploadToCloudinary  = (video) => {
     const formData = new FormData(form);
-    formData.append('ticket[video]', audio, 'my_audio.mp3');
+    formData.append('ticket[video]', video, 'my_video.mp4');
     Rails.ajax({
       url: "/tickets",
       type: "post",
@@ -68,7 +68,7 @@ const initRecordVideo = () => {
     })
     .then(() => startRecording(live.captureStream()))
     .then (recordedChunks => {
-      const recordedBlob = new Blob(recordedChunks, { type: "audio/mp3" });
+      const recordedBlob = new Blob(recordedChunks, { type: "video/mp4" });
       console.log(recordedBlob);
       uploadToCloudinary(recordedBlob);
     })
